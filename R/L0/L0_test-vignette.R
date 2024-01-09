9584061fe31e06d398990a777ed2acab6672f938
kellykapsar <kelly.kapsar@gmail.com>
2024-01-09 14:32:49 -0600

    Script for testing vignette.

--- R/L0/L0_test-vignette.R
+++ R/L0/L0_test-vignette.R
@@ -9,15 +9,76 @@
 # REQUIRES:         any scripts or code sources that are required
 # NOTES:            any additional information that is necessary / helpful to describe what needs to be done next
 
-# *Example:*
-# TITLE:            NEON Small Mammal Diversity Maps, Local to Regional
-# PROJECT:          "NEON's continental-scale biodiversity"
-# AUTHORS:          Quentin Read, Phoebe Zarnetske, Jon Knott
-# COLLABORATORS:    Sydne Record (Bryn Mawr), Ben Baiser (UFL), Angela Strecker (WWU), Kate Thibault (NEON)
-# DATA INPUT:       NEON organismal diversity data: all species, all years, all sites: neon_org_data_all.csv 
-#                   Range map data (IUCN) (iucn_mammal.shp)
-# DATA OUTPUT:      neon_mammal_richness_map.pdf, neon_mammal_richness.shp, multi_panel_mammal_plot.pdf 
-# DATE:             initiated: June 18, 2018; last run: 23 July 2020
-# OVERVIEW:         This script reads in NEON's small mammal diversity data, maps 2019 NEON diversity at NEON sites, maps background diversity across the US, and plots local (site or plot) vs. regional species pool relationships.
-# REQUIRES:           Small mammals: (1) L0_mammal_diversity_cleaning.R (2) L1_mammal_diversity.R
-# NOTES:              Lots of functions in here to pull out and reference because they will apply to each taxa. Add functionality to plot different years, and cumulative diversity across all years: requiring edit to L1_mammal_diversity.R
+# Load libraries 
+library(geodiv)
+# library(raster)
+# library(rasterVis)
+library(terra)
+library(mapdata)
+# library(maptools)
+# library(rgeos)
+library(tidyverse)
+library(parallel)
+library(sf)
+library(ggmap)
+library(corrplot)
+library(gridExtra)
+library(cowplot)
+library(factoextra)
+library(cluster)
+
+# Load vignette data 
+data(orforest) 
+orforest <- terra::unwrap(orforest)
+
+# Plot orforest without the trend removed
+eviCols <- colorRampPalette(c("lightyellow1", "darkgreen"))(100)
+
+terra::plot(orforest, 
+            col = eviCols, 
+            xlab = "Longitude", 
+            ylab = "Latitude", 
+            main = "orforest original")
+
+# Remove a polynomial trend
+orfor_rem <- remove_plane(orforest)
+
+# Calculate global metrics over the entire orforest image 
+(sa <- sa(orforest)) # average roughness
+(sbi <- sbi(orforest)) # surface bearing index
+(std <- std(orforest, create_plot = FALSE, option = 1))
+
+# Texture image creation using 'focal_metrics' function 
+window <- matrix(1, nrow = 7, ncol = 7)
+system.time(
+  output_raster <- focal_metrics(orforest, 
+                                 window, 
+                                 metrics = list('sa'), 
+                                 progress = TRUE)
+  )
+
+print(output_raster)
+
+terra::plot(output_raster$sa, 
+            col = eviCols, 
+            main = "sa")
+
+# Texture image creation using 'texture_image' function
+system.time(
+  output_raster2 <- texture_image(orforest, 
+                                  window_type = 'square',
+                                  size = 7, 
+                                  in_meters = FALSE, 
+                                  metric = 'sa', 
+                                  parallel = FALSE, 
+                                  nclumps = 100
+                                  )
+)
+
+# Values are different from output_raster
+print(output_raster2)
+
+terra::plot(output_raster2, # NOT WORKING - not sure why 
+            col = eviCols, 
+            main = "sa")
+
