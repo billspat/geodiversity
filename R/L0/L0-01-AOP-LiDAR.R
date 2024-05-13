291cee784f1e43cfec9620917e2b63aba491aedc
kellykapsar <kelly.kapsar@gmail.com>
2024-05-13 12:00:43 -0500

    Update with geodiv metric subset and "fixed" (except not) data download and mosaicking function.

--- R/L0/L0-01-AOP-LiDAR.R
+++ R/L0/L0-01-AOP-LiDAR.R
@@ -7,7 +7,7 @@
 # DATE:             March 2024
 # OVERVIEW:         This script contains code to download and generate raster mosaics of LiDAR data for NEON AOPs and calculate geodiversity metrics on them. 
 # REQUIRES:         config.R, forked version of NEON-data-skills repo (for mosiacking functions...)
-# NOTES:            
+# NOTES:            Bug in neonUtilities package was fixed (as of 13 May 2024). However, data download function still not working -- Error mesage: "URLs may have expired." 
 
 # Load libraries 
 library(geodiv)
@@ -20,11 +20,11 @@ library(neonUtilities)
 source("./R/config.R")
 
 # Load in functions from forked version of this repo: https://github.com/NEONScience/NEON-Data-Skills
-# source("../NEON-Data-Skills/tutorials/R/AOP/AOP-L3-rasters/aop_merge_raster_functions.R")
+source("../NEON-Data-Skills/tutorials/R/AOP/AOP-L3-rasters/aop_merge_raster_functions.R")
 # lapply(list.files("../NEON-utilities/neonUtilities/R/", pattern=".R", full.names=TRUE), source)
 
 # # Increase default timeout time 
-# options(timeout=300)
+options(timeout=300)
 
 # Specify input parameters 
 dpID <- "DP3.30024.001"
@@ -43,18 +43,18 @@ site_bounds <- sites[sites$siteID == siteCode,]
 # Based on this tutorial: https://www.neonscience.org/resources/learning-hub/tutorials/merge-aop-raster-data
 # makeFullSiteMosaics('DP3.30015.001','2021','MCRA',dataRootDir,outFileDir,NEON_TOKEN,include.provisional=T,COG=T)
 # 
-# makeFullSiteMosaics(dpID,
-#                     year,
-#                     siteCode,
-#                     dataRootDir,
-#                     outFileDir,
-#                     include.provisional = T,
-#                     apiToken = NEON_TOKEN,
-#                     COG = F)
+makeFullSiteMosaics(dpID,
+                    year,
+                    siteCode,
+                    dataRootDir,
+                    outFileDir,
+                    include.provisional = T,
+                    apiToken = NEON_TOKEN,# Loaded from config.R file
+                    COG = F)
 
 
 # # Revert to default timeout time 
-# options(timeout=60)
+options(timeout=60)
 # 
 # # calculate_geodiv <- function(site_name, site_bounds, DPID, metrics){
 # 
@@ -89,9 +89,9 @@ test <- terra::mask(v, l)
 #   mutate(test =is.na(min))
 
 # Define a list of input variables as strings
-metrics <- list("sa", "sq", "s10z", "sdq", "sdq6", "sdr", "sbi", "sci", "ssk", 
-                "sku", "sds", "sfd", "srw", "std", "svi", "stxr", "ssc", "sv", 
-                "sph", "sk", "smean", "svk", "spk", "scl", "sdc")
+# NOTE: std needs additional input of Option 2 to calculate stdi instead of std
+metrics <- list("sa", "sdq", "sbi", "ssk", 
+                "sku", "sfd", "std", "sds")
 
 # Define function to calculate time taken for a function
 time_function <- function(raster_layer, function_name) {
